9ead5d6855f773a7c6fc7132be3d98d4069c0f65
PMylon <pmylon20@gmail.com>
2016-06-06 11:01:25 +0300

    SPI communication between panstamp cores.

---
+++ Software/SPI_panstampMaster.ino
@@ -0,0 +1,97 @@
+/* Panstamp master sketch */
+/* Author: Panagiotis Mylonas
+ * Date: 6/6/2016
+ */
+
+/* Caveats: PIN10 MUST BE OUTPUT! */
+
+/* Headers */
+#include "HardwareSerial.h"
+#include <SPI.h>
+
+/* Type Definitions */
+typedef union imu_packet
+{
+  float f;
+  uint8_t b[4]; // float is 4-bytes on the Arduino platform.
+}imu_packet_t;
+
+/* Constants */
+const uint8_t PanstampSlavePin = 10; 
+const uint32_t transSpeed = 2000000; // panstamp core clock is 8MHz. SPI clock divisor is set to 4 (default).
+
+/* Globals */
+imu_packet_t impack;
+float yaw, pitch, roll;
+
+/* Function Prototypes */
+uint8_t SPI_transferWithDelay(const uint8_t _byte);
+
+void setup() {
+  Serial.begin(9600); // Used for debugging
+  // Set the Slave Select Pin as Output
+  pinMode(PanstampSlavePin, OUTPUT);
+  //digitalWrite(PanstampSlavePin, HIGH);  // ensure SS stays high for now
+  // Init SPI
+  SPI.begin();
+}
+
+void loop() {
+  /* Begin transmission */
+  SPI.beginTransaction(SPISettings(transSpeed, MSBFIRST, SPI_MODE0));
+  digitalWrite(PanstampSlavePin, LOW);
+  
+  /* Receive yaw */
+  
+  // Send to slave panstamp, request for Yaw.
+  SPI_transferWithDelay('Y');
+  impack.b[0] = SPI_transferWithDelay('1'); // reading byte0 (response from previous transfer). Initiate request for next byte.   
+  impack.b[1] = SPI_transferWithDelay('2'); // reading byte1
+  impack.b[2] = SPI_transferWithDelay('3'); // reading byte2
+  impack.b[3] = SPI_transferWithDelay(0); //  reading byte 3. Used to indicate termination of packet.
+  yaw = impack.f;
+  
+
+  /* Receive pitch */
+
+  // Send to slave panstamp, request for Pitch.
+  SPI_transferWithDelay('P');
+  impack.b[0] = SPI_transferWithDelay('1'); // reading byte0 (response from previous transfer). Initiate request for next byte.   
+  impack.b[1] = SPI_transferWithDelay('2'); // reading byte1
+  impack.b[2] = SPI_transferWithDelay('3'); // reading byte2
+  impack.b[3] = SPI_transferWithDelay(0); //  reading byte 3. Used to indicate termination of packet.
+  pitch = impack.f;
+  
+  /* Receive roll */
+
+  // Send to slave panstamp, request for Roll.
+  SPI_transferWithDelay('R');
+  impack.b[0] = SPI_transferWithDelay('1'); // reading byte0 (response from previous transfer). Initiate request for next byte.   
+  impack.b[1] = SPI_transferWithDelay('2'); // reading byte1
+  impack.b[2] = SPI_transferWithDelay('3'); // reading byte2
+  impack.b[3] = SPI_transferWithDelay(0); //  reading byte3. Used to indicate termination of packet.
+  roll = impack.f;
+  
+  /* End transmission */
+  digitalWrite(PanstampSlavePin, HIGH);
+  SPI.endTransaction();
+
+  /* Output imu packet to serial */
+  Serial.print("Yaw: ");
+  Serial.print(yaw);
+  Serial.print(" Pitch: ");
+  Serial.print(pitch);
+  Serial.print(" Roll: ");
+  Serial.println(roll);
+  // Use proper delay.
+  delay(100);
+}
+
+/* Functions */
+uint8_t SPI_transferWithDelay(const uint8_t _byte)
+{
+  uint8_t b = SPI.transfer (_byte);
+  delayMicroseconds (10);
+  return b;
+} 
+
---
+++ Software/SPI_panstampSlave.ino
@@ -0,0 +1,79 @@
+/* Panstamp slave sketch */
+/* Author: Panagiotis Mylonas
+ * Date: 6/6/2016
+ */
+
+/* TODO */
+//1) Put MCU into sleep until it wakes from interrupt, in order to conserve power.
+
+ /* Headers */
+#include "HardwareSerial.h"
+#include <SPI.h>
+
+/* Type Definitions */
+typedef union imu_packet
+{
+  float f;
+  uint8_t b[4]; // float is 4-bytes on the Arduino platform.
+}imu_packet_t;
+
+/* Globals */
+imu_packet_t impack;
+float yaw, pitch, roll;
+
+/* ISRs */
+// Called when incoming SPI data arrives
+ISR (SPI_STC_vect)
+{
+  uint8_t request = SPDR;
+
+  switch(request)
+  {
+    case 0:
+      impack.f = 0;
+      SPDR = 0;
+      break;
+    case 'Y': // Yaw request
+      impack.f = yaw;
+      SPDR = impack.b[0]; // Send 1st byte of yaw.
+      break;
+    case 'P': // Pitch request
+      impack.f = pitch;
+      SPDR = impack.b[0]; // Send 1st byte of pitch.
+      break;
+    case 'R': // Roll request
+      impack.f = roll;
+      SPDR = impack.b[0]; // Send 1st byte of roll.
+      break;
+    case '1': // Send 2nd byte.
+      SPDR = impack.b[1];
+      break;
+    case '2': // Send 3rd byte.
+      SPDR = impack.b[2];
+      break;
+    case '3': // Send fourth byte.
+      SPDR = impack.b[3];
+      break;  
+  }
+}
+void setup() {
+  // Enable SPI in slave mode.
+  SPCR |= bit(SPE);
+
+  //pinMode(MOSI, INPUT);
+  //pinMode(SCK, INPUT);
+  //pinMode(SS, INPUT); 
+  
+  pinMode(MISO, OUTPUT);
+
+  // Enable interrupt for SPI.
+  SPCR |= bit(SPIE);
+
+}
+
+void loop() {
+ // just for testing.
+ yaw = 45.23;
+ pitch = 18.32;
+ roll = -11.8934;
+}
