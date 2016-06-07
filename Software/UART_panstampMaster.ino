9c801edfe895b93f8749f25475c6c5a4bc5732be
PMylon <pmylon20@gmail.com>
2016-06-07 10:53:35 +0300

    Implement UART communication between the two MCUs.
    Master is the panstamp responsible for the execution of the central control algorithm, that implements the main hydrobot functionality (rf, navigation e.t.c)
    Slave is the panstamp responsible for the IMU/MARG sensor and the execution of the orientation algorithm.

---
+++ Software/UART_panstampMaster.ino
@@ -0,0 +1,47 @@
+/* Panstamp UART master sketch */
+/* Author: Panagiotis Mylonas
+ * Date: 6/6/2016
+ */
+
+/* Headers */
+#include "HardwareSerial.h"
+
+/* Type Definitions */
+typedef union imu_packet
+{
+  float f;
+  uint8_t b[4]; // float is 4-bytes on the Arduino platform.
+}imu_packet_t;
+
+/* Constants */
+
+/* Globals */
+imu_packet_t impack;
+float yaw, pitch, roll;
+
+void setup() {
+  Serial.begin(9600); 
+}
+
+void loop() {
+    
+  // Send to slave panstamp, request for Yaw, Pitch, Roll (Orientation).
+  Serial.write('O');
+  while (Serial.available() < 12); // 4bytes per euler angle
+  Serial.readBytes(impack.b, 4); 
+  yaw = impack.f;
+  Serial.readBytes(impack.b, 4);
+  pitch = impack.f;
+  Serial.readBytes(impack.b, 4);
+  roll = impack.f;
+ 
+  /* Output imu packet to serial */
+  Serial.print("Yaw: ");
+  Serial.print(yaw);
+  Serial.print(" Pitch: ");
+  Serial.print(pitch);
+  Serial.print(" Roll: ");
+  Serial.println(roll);
+  // Use proper delay.
+  delay(1000);
+}
---
+++ Software/UART_panstampSlave.ino
@@ -0,0 +1,52 @@
+/* Panstamp UART slave sketch */
+/* Author: Panagiotis Mylonas
+ * Date: 6/6/2016
+ */
+
+/* TODO */
+//1) Put MCU into sleep until it wakes from interrupt, in order to conserve power.
+
+ /* Headers */
+#include "HardwareSerial.h"
+
+/* Type Definitions */
+typedef union imu_packet
+{
+  float f;
+  uint8_t b[4]; // float is 4-bytes on the Arduino platform.
+}imu_packet_t;
+
+/* Globals */
+imu_packet_t impack;
+float yaw, pitch, roll;
+uint8_t request;
+
+/* ISRs */
+// See: https://forum.arduino.cc/index.php?topic=349921.0
+
+void setup() {
+ // just for testing.
+ yaw = 45.23;
+ pitch = 18.32;
+ roll = -11.8934;
+ Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0)
+  {
+    request = Serial.read();
+    if (request == 'O') // Orientation
+    {
+      // getOrientation. 
+      // Send yaw, pitch and roll
+      impack.f = yaw;
+      Serial.write(impack.b, 4);
+      impack.f = pitch;
+      Serial.write(impack.b, 4);
+      impack.f = roll;
+      Serial.write(impack.b, 4);
+    }
+  }
+}
+
